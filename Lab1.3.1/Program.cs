af3370e0b94d99d3899c6958e32d9ccc585b5416
Born2drum <47678190+Born2drum@users.noreply.github.com>
2019-12-15 00:34:40 +0200

    third commit

---
+++ Lab1.3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab1._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; ; i++)
+            {
+                Console.WriteLine("Enter your steps ");
+                double a = double.Parse(Console.ReadLine());
+                if (a == 3827183 || a == 3817283)
+                {
+                    Console.WriteLine("It is the right order!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                    Console.WriteLine("If you want to exit press 1. To try again press any number");
+                    
+                    byte b = byte.Parse(Console.ReadLine());
+                    if (b == 1)
+                    {
+                        break;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
